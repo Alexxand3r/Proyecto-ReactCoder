2b13f9bcd2197d813b8b1da241ceb5e165a6cacc
Alexander De Simone <cristianoali08i@gmail.com>
2022-08-26 17:15:03 -0300

    NavBarcorrecciones

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -14,13 +14,23 @@ const NavBar = () => {
             Mi Carrito Coder
           </Navbar.Brand>
           <Nav className="d-flex justify-content-center fs-3">
-            <Nav.Link href="#home">Inicio</Nav.Link>
-            <Nav.Link href="#features">Productos</Nav.Link>
-            <Nav.Link href="#pricing">Nosotros</Nav.Link>
+            <Nav.Link href="/#">Inicio</Nav.Link>
+            <Nav.Link href="/#">Productos</Nav.Link>
+            <Nav.Link href="/#">Nosotros</Nav.Link>
           </Nav>
           <Nav>
-            <Button variant="outline-info me-2">login</Button>
-            <Button variant="outline-info me-3">registrate</Button>
+            <Button variant="outline-info me-3">
+              {" "}
+              <i className="fa fa-shopping-cart me-1"></i>Cart ( 0 )
+            </Button>
+            <Button variant="outline-info me-2">
+              {" "}
+              <i className="fa fa-sign-in me-1"></i>Login
+            </Button>
+            <Button variant="outline-info me-3">
+              {" "}
+              <i className="fa fa-user-plus me-1"></i>Registrate
+            </Button>
 
             <Form className="d-flex me-4">
               <Form.Control
